dde1f4b3ea166373bbcaf5639360e8bfc8446295
mani1911 <mail2aj19@gmail.com>
2022-06-13 21:11:11 +0530

    tested a working logic

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ let questionWord = "";
 let maxWrong = 6;
 let answer = "";
 
+const replaceChar = (origString, replaceChar, index)=>{
+    let firstPart = origString.substr(0, index);
+    let lastPart = origString.substr(index + 1);
+      
+    let newString = firstPart + replaceChar + lastPart;
+    return newString;
+}
+
 const setRandomWord = async ()=>{
     let res = await fetch('https://random-word-api.herokuapp.com/word');
     let data = await res.json();
@@ -20,13 +28,20 @@ const setRandomWord = async ()=>{
 
 const checkLetter = (l)=>{
     if(questionWord.includes(l)){
-        
+        let ans1 = answerBlock.innerHTML;
+        for(let i=0; i<questionWord.length; i++){
+            ans1 = answerBlock.innerHTML;
+             if(questionWord[i] == l){
+                ans1 = replaceChar(ans1, l, 3*i+1);
+                ans1 = replaceChar(ans1, " ", 3*i+2);
+                answerBlock.innerHTML = ans1;
+             }
+        }
     }
     else{
         wrongs++;
         wrongCount.innerHTML = wrongs;
         image.src = `images/${wrongs}.jpg`;
-
     }
 }
 
@@ -51,8 +66,9 @@ const handleAnswerBlock = async ()=>{
     }
 }
 
-createLetterBtns();
 handleAnswerBlock();
+
+createLetterBtns();
 
 
 
